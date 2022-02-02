7a4735e8d3d7eae555f1fac139186698031241a1
hann <guanghan.wu@outlook.com>
2022-02-03 02:36:32 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -53,5 +53,3 @@ export MANPATH="$MANPATH:/usr/local/texlive/2021/texmf-dist/doc/man"
 export INFOPATH="$INFOPATH:/usr/local/texlive/2021/texmf-dist/doc/info"
 export PATH="/usr/local/texlive/2021/bin/x86_64-linux:$PATH"
 
-sh ~/wsl_clash.sh
-cd ~
